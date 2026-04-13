86bca1ebe8099408d5372c2523e48413a45223b7
Merve Kindan <mervekanmaz10@gmail.com>
2026-04-14 00:03:52 +0200

    Add our mission section

--- courses/frontend/react/space-galactica-app/src/pages/AboutUsPage/AboutUsPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -69,6 +69,18 @@ export const Crew = () => {
             <main className="mainContent">
                 <h1>About us</h1>
                 <section className="card">
+                    <h2>Our Mission</h2>
+                    <p>
+                        At Galactica, our mission is to unlock the wonders of
+                        the universe for everyone. We believe that space is the
+                        final frontier and that the opportunity to explore it
+                        should be within everyone’s reach. Our journeys are
+                        designed to inspire, educate, and provide a
+                        once-in-a-lifetime experience that transcends the
+                        ordinary.
+                    </p>
+                </section>
+                <section className="card">
                     <h2>Our Values</h2>
                     <OurValues />
                 </section>

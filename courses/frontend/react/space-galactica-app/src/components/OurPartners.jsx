7bc1fba9a1e004f0f84057114b7c7c8e122859d7
Merve Kindan <mervekanmaz10@gmail.com>
2026-04-14 15:42:15 +0200

    Move the  OurValues, OurCrew, and OurPartners components to the components folder and refactor the AboutUsPage component

---
+++ courses/frontend/react/space-galactica-app/src/components/OurPartners.jsx
@@ -0,0 +1,40 @@
+import styles from "./OurPartners.module.css";
+import { PartnerLogo } from "./PartnerLogo";
+
+const partners = [
+    {
+        id: 1,
+        alt: "alphabet-logo",
+        src: "/business_partners/alphabet-logo.png",
+    },
+    { id: 2, alt: "amazon-logo", src: "/business_partners/amazon_logo.png" },
+    { id: 3, alt: "cbc-logo", src: "/business_partners/CBC_Logo_White.png" },
+    {
+        id: 4,
+        alt: "microsoft-logo",
+        src: "/business_partners/Microsoft-Logo-white.png",
+    },
+    { id: 5, alt: "nyu-logo", src: "/business_partners/nyu-logo.png" },
+    {
+        id: 6,
+        alt: "queens-logo",
+        src: "/business_partners/QueensLogo_white.png",
+    },
+    { id: 7, alt: "samsung-logo", src: "/business_partners/samsung-logo.png" },
+    { id: 8, alt: "sodexo-logo", src: "/business_partners/sodexo-logo.png" },
+];
+
+export const OurPartners = () => {
+    // 🧑🏽‍🚀 Task - Week 1
+    // Create the "Our Partners section".
+    // Use the descriptions provided in /src/pages/AboutUsPage/README.md.
+    // Use the pictures from /public/business_partners.
+    // Some inspiration ideas can be found in /data/inspiration_about_us.
+    return (
+        <div className={styles.partnerLogosContainer}>
+            {partners.map((partner) => (
+                <PartnerLogo key={partner.id} {...partner} />
+            ))}
+        </div>
+    );
+};
